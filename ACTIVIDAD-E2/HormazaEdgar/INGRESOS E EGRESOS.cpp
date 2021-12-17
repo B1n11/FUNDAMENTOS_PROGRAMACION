7145f1039e68a9cfe099d474bec9172a9b1eb399
Edgar Hormaza <58125743+B1n11@users.noreply.github.com>
2021-12-17 10:15:12 -0500

    Update INGRESOS E EGRESOS.cpp

--- ACTIVIDAD-E2/HormazaEdgar/INGRESOS E EGRESOS.cpp
+++ ACTIVIDAD-E2/HormazaEdgar/INGRESOS E EGRESOS.cpp
@@ -1,32 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+int main()
+{
+float x,c=0,li,a=0,i=0,g=0;
+cout<<"Cuantos valores va ingresar: "; cin>>li;
+do{
+cout<<"Ingrese los valores: "; cin>>x;
+c=c+1;
+a=a+x;
+if(x>0){
+	i=i+x;
+}else{
+	g=g+x;
+}
+}while(c<li);
+cout<<"El saldo de egresos es: "<<g<<endl;
+cout<<"El saldo de ingresos es: "<<i<<endl;
+cout<<"El saldo total es: "<<a<<endl;
 
-
- int main(){
- 	
- 	int x,c=0,li,a=0,i=0,g=0;
- 	
- 	cout<<"limite:"; cin>>li;
- 	cout<<"ingrese los valores:"; cin>>x;
- 	
-     c=c+1;
- 	a=a+x;
- 	
- 	if(x>=0)
- 	{
-	 i=i+x
- 	}
- 	if(x<=0)
- 	{
- 	   g=g+x
- 	}  
- 	if(c<li)
- 	{
- 	cout<<"ingrese los valores:"; cin>>x;
- 	}
- 	if(c>li)
- 	{
- 	cout<<g,i,a<<endl;
- 	}
- 	return 0;
- }
+}
