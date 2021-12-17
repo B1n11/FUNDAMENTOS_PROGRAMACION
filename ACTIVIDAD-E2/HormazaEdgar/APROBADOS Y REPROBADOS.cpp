b4bf3363b1633d0f6a658ba61ee60eb667c1443e
Edgar Hormaza <58125743+B1n11@users.noreply.github.com>
2021-12-17 10:08:30 -0500

    Update APROBADOS Y REPROBADOS.cpp

--- ACTIVIDAD-E2/HormazaEdgar/APROBADOS Y REPROBADOS.cpp
+++ ACTIVIDAD-E2/HormazaEdgar/APROBADOS Y REPROBADOS.cpp
@@ -1,41 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-
- int main(){
-	
-	int pr,c=0,li=30,ap=0,ar=0;
-	
-cout<<"ingrese promedios:"; 
- cin>>pr;
-	c=c+1;
-
-if(pr >= 7)
+int main()
 {
+float pr,c=0,li=30,ap=0,ar=0;
+do{
+cout<<"Ingrese el promedio: "; cin>>pr;
+c=c+1;
+if(pr>7){
 	ap=ap+1;
+}else{
+	if(pr<5){
+		
+		ar=ar+1;
+	}else{}
 }
-if (pr <= 7)
-{
-if (pr <= 5)
-{
-	ar=ar+1;
-}
-}
-	if(c<li)
-	{
-cout<<"ingrese promedios:"; 
- cin>>pr;
-	}
-	if(c>li)
-	{
-		cout<<ap,ar;
-	}
-	
-
-
-
-
-
-
-return 0;
+}while(c<li1
+);
+cout<<"Aprueban "<<ap<<endl;
+cout<<"Reprueban "<<ar<<endl;
+return (0) ;
 }
