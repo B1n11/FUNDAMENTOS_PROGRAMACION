4a2987a01063ee71784a8c126e93bbfffd68424a
Edgar Hormaza <58125743+B1n11@users.noreply.github.com>
2021-12-19 00:08:19 -0500

    Add files via upload

---
+++ ACTIVIDAD-C2/Cecia Bautista/suma simple.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+	
+	float a,b,c;
+	
+	cout<<"ingrese numero 1:"<<endl;
+	cin>>a;
+	
+	
+	cout<<"ingrese numero 2:"<<endl;
+	cin>>b;
+	
+	c=a+b;
+	
+	cout<<"el resultado de la suma es : "<<c<<endl;
+	
+	
+	
+	return 0;
+}
